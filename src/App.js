27ca4909d42f3d4625dc8a35f34a5c14669fd6ee
twozeronine <amdusias209@gmail.com>
2021-02-16 15:36:28 +0900

    Add size props - Button component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from "react";
 import Button from "./components/Button";
+import "./App.scss";
 
 function App() {
   return (
     <div className="App">
       <div className="buttons">
+        <Button size="large">Button</Button>
         <Button>Button</Button>
+        <Button size="small">Button</Button>
       </div>
     </div>
   );
--- src/components/Button.js
+++ src/components/Button.js
@@ -1,8 +1,15 @@
 import React from "react";
+import classnames from "classnames";
+
 import "./Button.scss";
 
-function Button({ children }) {
-  return <button className="Button">{children}</button>;
+// size : large , medium , small
+function Button({ children, size }) {
+  return <button className={classnames("Button", size)}>{children}</button>;
 }
+
+Button.defaultProps = {
+  size: "medium",
+};
 
 export default Button;
